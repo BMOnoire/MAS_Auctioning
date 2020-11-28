c35850ce90ad95c27af0aaba1f08645e655bdbd6
Luca <luca.95b@live.it>
2020-11-28 14:57:47 +0100

    levelled commitment

--- main.py
+++ main.py
@@ -72,7 +72,75 @@ def launch_new_test(id, n_buyers, n_sellers, n_rounds, max_starting_price, max_b
                 market_price_list.append(market_price)
                 seller_profit_list.append(seller_profit)
                 buyer_profit_list.append(winner_profit)
+    if type == "LEVELED_COMMITMENT":
+        # init AGENTS
+        seller_list = [seller.Seller(i) for i in range(n_sellers)]
+        buyer_list  = [buyer.Buyer(i, seller_list, max_bidding_factor) for i in range(n_buyers)]
 
+
+        for round in range(n_rounds):
+            buyers_won_auction = {}
+            # created a shuffled copy of the buyer_list and seller_list every round
+            sellers = random.sample(seller_list, len(seller_list))
+            buyers  = random.sample(buyer_list, len(buyer_list))
+
+
+            # start the auctions
+            for slr in sellers:
+
+                seller_price = slr.init_random_starting_price(max_starting_price)
+
+
+                # the buyers start the bids
+                bid_list = []
+                for bur in buyers:
+                    buyer_bid = bur.get_bidding_factor(slr.id) * seller_price  # make the bid thanks to the factor given by the factor list
+
+                    bid_list.append(buyer_bid) # note: this bids have the same order of buyer_turn_list
+
+                # bid end
+                market_price = sum(bid_list)/len(bid_list) # avg of all the bids
+
+                bid_list = [ (bid if bid <= market_price else 0) for bid in bid_list]  # remove the values over the market_price
+                winner_index = bid_list.index(max(bid_list)) # found the first winner, the other ones... NO
+                bid_list[winner_index] = 0 # removed is offer...
+                winner_payment = np.amax(bid_list) # ...to pick the second max
+
+                # profit for sellers
+                seller_profit = winner_payment - seller_price
+
+
+
+                # profit for buyers
+                # winner = buyers.pop(winner_index)  # remove the winner from the other auctions
+
+                winner_profit = market_price - winner_payment
+                # add the buyer id inside the dict, if the key already exists, it return the previous value
+                prev_auction = buyers_won_auction.setdefault(winner.id,(slr.id,winner_payment, winner_profit))
+                penalty_fee=0
+                if prev_auction[0] != slr.id:
+                    if winner_profit > prev_auction[2]:
+                        penalty_fee = epsilon * prev_auction[1]
+                    else:
+                        penalty_fee = epsilon * winner_payment
+
+                for real_slr in seller_list:  # update the seller profit, Note: the real list
+                    if real_slr.id == slr.id:
+                        real_slr.add_to_profit(seller_profit)
+                    if real_slr.id == prev_auction[0]:
+                        real_slr.add_to_profit(penalty_fee)
+
+
+                for real_bur in buyer_list:  # update the buyer profit, Note: the real list
+                    if real_bur.id == winner.id:
+                        real_bur.add_to_profit(winner_profit)
+                    if real_bur.id == prev_auction[0]:
+                        real_bur.add_to_profit(-penalty_fee)
+
+
+                market_price_list.append(market_price)
+                seller_profit_list.append(seller_profit)
+                buyer_profit_list.append(winner_profit)
 
 
 
