357ca9ac734ccd6b06dd4163c9b3e8540cdba673
Glauco <glauco.lorenzut@gmail.com>
2020-12-12 19:10:11 +0100

    TEST - First confront

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SEED = 1
+SEED = 2
 
 ROUNDS = 1000
 TIMES = 10
@@ -10,12 +10,12 @@ STEP_PLOTTING = None
 # LEVELED_COMMITMENT_AUCTIONING
 # PURE_AUCTIONING
 
-TEST_TITLE = "Variation Bidding Factor Decrease (Leveled Commitment)"
+TEST_TITLE = "Confront Auctioning Types"
 TYPE = "LEVELED_COMMITMENT_AUCTIONING"
 
 test_list = [
     {
-        "id": "Bid Factor Decrease 02-04",
+        "id": "pure",
         "execute": True,
         "times": TIMES,
         "n_buyers": 4,
@@ -23,17 +23,17 @@ test_list = [
         "n_rounds": ROUNDS,
         "max_starting_price": 10,
         "range_bidding_factor_increase": [1, 2],
-        "range_bidding_factor_decrease": [0.2, 0.4],
+        "range_bidding_factor_decrease": [0, 1],
         "max_bidding_factor": 2,
         "epsilon": 0.2,
-        "type": TYPE,
+        "type": "PURE_AUCTIONING",
         "params": {
             "BIDDING_STRATEGY": True,
             "SELLER_STRATEGY": None
         }
     },
     {
-        "id": "Bid Factor Decrease 04-06",
+        "id": "leveled commitment",
         "execute": True,
         "times": TIMES,
         "n_buyers": 4,
@@ -41,43 +41,7 @@ test_list = [
         "n_rounds": ROUNDS,
         "max_starting_price": 10,
         "range_bidding_factor_increase": [1, 2],
-        "range_bidding_factor_decrease": [0.4, 0.6],
-        "max_bidding_factor": 2,
-        "epsilon": 0.2,
-        "type": TYPE,
-        "params": {
-            "BIDDING_STRATEGY": True,
-            "SELLER_STRATEGY": None
-        }
-    },
-    {
-        "id": "Bid Factor Decrease 06-08",
-        "execute": True,
-        "times": TIMES,
-        "n_buyers": 4,
-        "n_sellers": 3,
-        "n_rounds": ROUNDS,
-        "max_starting_price": 10,
-        "range_bidding_factor_increase": [1, 2],
-        "range_bidding_factor_decrease": [0.6, 0.8],
-        "max_bidding_factor": 2,
-        "epsilon": 0.2,
-        "type": TYPE,
-        "params": {
-            "BIDDING_STRATEGY": True,
-            "SELLER_STRATEGY": None
-        }
-    },
-    {
-        "id": "Bid Factor Decrease 08-10",
-        "execute": True,
-        "times": TIMES,
-        "n_buyers": 4,
-        "n_sellers": 3,
-        "n_rounds": ROUNDS,
-        "max_starting_price": 10,
-        "range_bidding_factor_increase": [1, 2],
-        "range_bidding_factor_decrease": [0.8, 1],
+        "range_bidding_factor_decrease": [0, 1],
         "max_bidding_factor": 2,
         "epsilon": 0.2,
         "type": TYPE,
