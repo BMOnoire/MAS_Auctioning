ae7ea095d3e358bbd652aec08385470bd5338bbc
Glauco <glauco.lorenzut@gmail.com>
2020-12-13 21:36:05 +0100

    Delete DELETE_ME

--- DELETE_ME/auction_type.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class AuctionType(Enum):
-    PURE = 1
-    LEVELED_COMMITMENT = 2
--- DELETE_ME/bid_history_entry.py
+++
@@ -1,6 +0,0 @@
-class BidHistoryEntry:
-
-    def __init__(self, seller, market_price, price):
-        self.seller = seller
-        self.market_price = market_price
-        self.price = price
